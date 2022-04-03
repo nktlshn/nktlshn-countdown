779c8b31b0c1dae85881719ec995f4c13ce9c69b
lshn <lushinnv@gmail.com>
2022-04-03 23:11:38 +0300

    first

---
+++ main.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+from telegram.ext import Updater
+import random
+import emojis_list
+from cfg import TG_TOKEN, GROUP_ID, DAY_X
+
+TODAY = datetime.now()
+
+
+def get_time_delta():
+    day_x = datetime.strptime(DAY_X, '%d%m%Y')
+    delta_days = TODAY - day_x
+
+    return abs(delta_days.days)
+
+
+def not_weekend():
+    if TODAY.weekday() not in (5, 6):
+        return True
+    else:
+        return False
+
+
+days_left = get_time_delta()
+
+
+def send_message_to_tg():
+    random_emoji = random.choice(emojis_list.emoji_list)
+
+    updater = Updater(TG_TOKEN)
+    updater.bot.send_message(chat_id=GROUP_ID,
+                             text=f'Дней до ОПВ: {days_left}' + f' {random_emoji}')
+    print('sending...')
+
+
+if __name__ == '__main__' and not_weekend():
+    send_message_to_tg()
